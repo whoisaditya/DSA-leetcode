b81ba0f06b52ff862e85d097b8b5fa5375383c65
Aditya Mitra <mitraaditya1357@gmail.com>
2022-06-27 17:59:57 +0530

    chore: completed day 1

---
+++ Day-1/1.java
@@ -0,0 +1,19 @@
+// 1480. Running Sum of 1d Array
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+// Return the running sum of nums.
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        result[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            int sum = 0;
+            for (int j = 0; j <= i; j++) {
+                sum += nums[j];
+            }
+            result[i] = sum;
+        }
+        return result;
+    }
+}
